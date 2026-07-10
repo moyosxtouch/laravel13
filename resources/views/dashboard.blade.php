b276e10dea7e6eccee8728a4970f08f3d1444578
moyo <moyosxtouch@gmail.com>
2026-07-10 16:13:11 -0600

    se creo el layout del dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,20 @@
-@extends("layouts.auth")
+@extends("layouts.app")
 @section ("title")
 Administra tus Presupuestos
 @endsection
-@section("auth-contents")
-@if(session("success"))
-<p class="my-10 text-center border border-green-400 bg-green-100 py-3 text-green-700 text-sm">{{ session("success") }}</p>
-@endif
+@section("actions")
+<div class="sm:flex sm:items-center mt-10">
+    <div class="sm:flex-auto">
+        <h1 class="font-bold text-4xl">Administra tus Presupuestos</h1>
+        <p class="mt-2 text-xl text-gray-500">Administra tus Presupuestos en esta sección</p>
+    </div>
+    <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+        <a href=""
+            class="block bg-amber-500 text-white w-full px-5 py-3 rounded-lg  font-bold  text-xl cursor-pointer text-center">Nuevo Presupuesto</a>
+    </div>
+</div>
+@endsection
+
+@section("dashboard-contents")
+
 @endsection
